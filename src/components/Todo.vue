0b8d385a2f84a48bbe210623d402485946d29ed2
Anologicon <pauloraimundi2009@gmail.com>
2018-10-05 13:18:56 -0300

    View

---
+++ src/components/Todo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="todo">
+    <h1>What i do now?</h1>
+    <div class="container">
+      <div class="box-input">
+        <form @submit="save">
+          <input type="text" placeholder="I have make a ..." v-model="todoName" id="todo-name" @keypress="typing = true">
+          <span class="typing" v-show="typing">Press enter to save ...</span>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo',
+  data() {
+    return {
+      todoName: '',
+      typing: false,
+    };
+  },
+};
+</script>
+
+<style>
+
+  h1 {
+    font-weight: 400;
+    font-size: 2.2em;
+    color: rgba(0, 0, 0, 0.69);
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  input {
+    display: flex;
+    width: 300px;
+     border: 5px solid rgba(168, 172, 255, 0.49019607843137253);; 
+    -webkit-box-shadow: 
+      inset 0 0 8px  rgba(0,0,0,0.1),
+            0 0 16px rgba(0,0,0,0.1); 
+    -moz-box-shadow: 
+      inset 0 0 8px  rgba(0,0,0,0.1),
+            0 0 16px rgba(0,0,0,0.1); 
+    box-shadow: 
+      inset 0 0 8px  rgba(0,0,0,0.1),
+            0 0 16px rgba(0,0,0,0.1); 
+    padding: 15px;
+    background: rgba(255,255,255,0.5);
+    margin: 0 0 10px 0;
+  }
+  
+  .typing {
+    font-style: italic;
+    font-size: 0.8em;
+    box-shadow: inset 0 0 8px rgba(0,0,0,0.1), 0 0 16px rgba(0,0,0,0.1);
+  }
+
+</style>
+
+
