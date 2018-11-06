209383f889b54b429266195356179b545e0c875d
Anologicon <pauloraimundi2009@gmail.com>
2018-11-06 21:07:13 -0200

    NOT NPM MAN!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <header>
-      <span>A nice todo List - With MENV stacknpm </span>
+      <span>A nice todo List - With MENV stack </span>
     </header>
     <main>
       <router-view></router-view>
